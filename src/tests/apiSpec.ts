0d4b49247b6a433e3807b99c8b921c5584130d7e
amribrahim34 <amramr3434@gmail.com>
2022-01-19 13:46:16 +0200

    first test is working

--- src/tests/apiSpec.ts
+++ src/tests/apiSpec.ts
@@ -1,13 +1,24 @@
-import express from 'express';
-const app = express.Router();
-
-function get_image() {
-    app.get('http://127.0.0.1:3000/api/images?filename=icelandwaterfall&width=200&height=200', (req, res) => {
-        return res.status;
-    });
+// creating type for json
+interface Json {
+    [x: string]: string | number | boolean | Date | Json | JsonArray;
 }
+interface JsonArray extends Array<string | number | boolean | Date | Json | JsonArray> { }
+
+async function get_image() {
+    await fetch('http://127.0.0.1:3000/api/images?filename=icelandwaterfall&width=200&height=200', {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then(
+        (response) => {
+            return response.status;
+        }
+    );
+}
+
 describe("test suite ", () => {
     it("test ", () => {
-        expect(get_image()).toBe();
+        expect(get_image).toEqual(200);
     });
 });
