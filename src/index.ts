d4d523cb67393820987e8e2987e830ac79c53378
amribrahim34 <amramr3434@gmail.com>
2022-01-25 00:48:30 +0200

    refactoring index.ts and removing dev dependancies from dependancies in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import express from 'express';
-import fs from 'fs';
-import { make_image } from './utilities';
 import routes from './routes/index';
 
-const sharp = require('sharp');
 
 const app = express();
 const port = 3000;
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
 app.use('/cached', express.static('cached'));
-
-
-
 
 app.use('/api', routes);
 
