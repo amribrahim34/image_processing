49a46110d1833e27fe4df59efe259e73808e3ef8
amribrahim34 <amramr3434@gmail.com>
2022-01-25 00:43:32 +0200

    successfully separated the routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import fs from 'fs';
 import { make_image } from './utilities';
+import routes from './routes/index';
 
 const sharp = require('sharp');
 
@@ -13,20 +14,7 @@ app.use('/cached', express.static('cached'));
 
 
 
-app.get('/api/images', (req, res) => {
-    let width = Number(req.query.width);
-    let height = Number(req.query.height);
-    let filename = String(req.query.filename);
-    let path: string = `http://localhost:${port}/cached/${filename}_${height}_${width}.jpg`;
-    let image = `<img src="${path}" alt="${filename}">`;
-    let oprtn = make_image(width, height, filename);
-    if (oprtn == 'success' || oprtn == true) {
-        res.status(200);
-        res.send(image);
-    } else {
-        res.send("error occurred");
-    }
-});
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import { make_image } from '../../utilities';
+
+const port = 3000;
 
 const images = express.Router();
-images.get('/images', (req, res) => {
-    res.status(200);
-    res.send('done');
+images.get('/', (req, res) => {
+    let width = Number(req.query.width);
+    let height = Number(req.query.height);
+    let filename = String(req.query.filename);
+    let path: string = `http://localhost:${port}/cached/${filename}_${height}_${width}.jpg`;
+    let image = `<img src="${path}" alt="${filename}">`;
+    let oprtn = make_image(width, height, filename);
+    if (oprtn == 'success' || oprtn == true) {
+        res.status(200);
+        res.send(image);
+    } else {
+        res.send("error occurred");
+    }
 });
 
 export default images;
