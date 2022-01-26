f41ca1fe6091e2c0c2c259ac898f0d0bf8de5c52
amribrahim34 <amramr3434@gmail.com>
2022-01-26 20:08:24 +0200

    testing the endpoint , installed dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 import express from "express";
 import routes from "./routes/index";
+import 'dotenv/config';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 app.use(express.static("public"));
 app.use("/images", express.static("images"));
 app.use("/cached", express.static("cached"));
 
 app.use("/api", routes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (req: any, res: any): void => {
+  res.status(200).send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`server started on http://localhost:${port}`);
-});
+if (process.env.APP_ENV != 'testing') {
+  app.listen(port, () => {
+    console.log(`server started on http://localhost:${port}`);
+  });
+}
+
 
 export default app;
--- src/tests/apiSpec.ts
+++ src/tests/apiSpec.ts
@@ -1,6 +1,11 @@
 import { make_image } from "../utilities";
+import supertest from "supertest";
+import app from "../index"
 
-describe("make image function tests - ", () => {
+let base_url = "http://localhost:3000/"
+const request = supertest;
+
+describe("test image function - ", () => {
   it("test if the function work", () => {
     make_image(200, 200, "icelandwaterfall").then((out) => {
       expect(out).toBe(true);
@@ -12,4 +17,18 @@ describe("make image function tests - ", () => {
       expect(out).not.toBe(true);
     });
   });
+
 });
+
+describe("test API endpoints ", () => {
+
+  it("test that / return code 200", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(200);
+  });
+
+  it("test the actual API return code 200", async () => {
+    const res = await request(app).get("/api/images?filename=fjord&width=200&height=300");
+    expect(res.statusCode).toBe(200);
+  });
+})
